9664fc7c6c2099261a38b1a7bc94019226b88aac
sunshowers <rain@sunshowers.io>
2025-09-29 18:15:08 +0000

    Deploying to gh-pages from @ camino-rs/camino@28e15dbd8a51c086d244e70f7f77eeee7d0b25cb 🚀

---
+++ rustdoc/trait.impl/core/fmt/trait.UpperExp.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rustix",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.UpperExp.html\" title=\"trait core::fmt::UpperExp\">UpperExp</a> for <a class=\"struct\" href=\"rustix/fs/struct.Gid.html\" title=\"struct rustix::fs::Gid\">Gid</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.UpperExp.html\" title=\"trait core::fmt::UpperExp\">UpperExp</a> for <a class=\"struct\" href=\"rustix/fs/struct.Uid.html\" title=\"struct rustix::fs::Uid\">Uid</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[508]}
---
+++ rustdoc/trait.impl/core/marker/trait.Sized.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["camino",[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"camino/struct.Utf8Path.html\" title=\"struct camino::Utf8Path\">Utf8Path</a>",1,["camino::Utf8Path"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[289]}
