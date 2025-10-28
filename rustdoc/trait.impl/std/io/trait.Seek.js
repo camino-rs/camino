e379486883bb5972e3ae23aba16ef11ff841995d
sunshowers <rain@sunshowers.io>
2025-10-28 01:43:18 +0000

    Deploying to gh-pages from @ camino-rs/camino@73c7a362a86cb3bc31f0c736973515f5ad056b63 🚀

---
+++ rustdoc/trait.impl/std/io/trait.Seek.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["tempfile",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/std/io/trait.Seek.html\" title=\"trait std::io::Seek\">Seek</a> for &amp;<a class=\"struct\" href=\"tempfile/struct.NamedTempFile.html\" title=\"struct tempfile::NamedTempFile\">NamedTempFile</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.90.0/std/fs/struct.File.html\" title=\"struct std::fs::File\">File</a>&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/std/io/trait.Seek.html\" title=\"trait std::io::Seek\">Seek</a> for <a class=\"struct\" href=\"tempfile/struct.SpooledTempFile.html\" title=\"struct tempfile::SpooledTempFile\">SpooledTempFile</a>"],["impl&lt;F: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/std/io/trait.Seek.html\" title=\"trait std::io::Seek\">Seek</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/std/io/trait.Seek.html\" title=\"trait std::io::Seek\">Seek</a> for <a class=\"struct\" href=\"tempfile/struct.NamedTempFile.html\" title=\"struct tempfile::NamedTempFile\">NamedTempFile</a>&lt;F&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1077]}
